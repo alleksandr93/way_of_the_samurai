ba3463a79005589ad5a4badfd1da1ba3827c29c4
Aleksandr <aleksandr-ishutin93@mail.ru>
2024-05-10 23:55:02 +0400

    Lesson 07

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <Header />
+            <Technologies />
+        </div>
+    );
+}
+export default App;
+
+const Technologies = () => {
+    return <ul>
+        <li>html</li>
+        <li>css</li>
+        <li>js</li>
+        <li>reack</li>
+
+    </ul>
 }
 
-export default App;
+const Header = () => {
+    return <div>
+        <a href="#">Home</a>
+        <a href="#">New Feed</a>
+        <a href="#">Messages</a>
+    </div>
+}
+
