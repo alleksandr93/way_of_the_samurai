e5ef23ccc933fb641579a748ef5cc5b08bbdcbd3
Aleksandr <aleksandr-ishutin93@mail.ru>
2024-05-11 15:14:58 +0400

    lesson 12

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from '../img/logo.webp';
+export const Header = () => {
+    return (
+            <header className={'header'}>
+                <img src={logo} alt="Картиночка"/>
+            </header>
+
+    );
+};
+
---
+++ src/components/Profile.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export const Profile = () => {
+    return <div className={'content'}>
+        <div>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9WAOOKFMTpyptDHLj3VGsxzg7tz4H1G4ijT3HodkbPNDIl4tL91j9lAX6Y6T2QnxOWG8&usqp=CAU"
+                alt=""/>
+        </div>
+        <div>
+            ava+decrp
+        </div>
+        <div>
+            My post
+        </div>
+        <div>
+            New post
+        </div>
+        <div>
+            <div>post1</div>
+            <div>post2</div>
+        </div>
+    </div>
+
+        ;
+};
+
